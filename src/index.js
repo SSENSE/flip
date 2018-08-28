23270220e445bc8ed7c3aa38c3c4e3ab90886bf7
Quinn Langille <quinn@quinnlangille.com>
2018-08-28 17:42:46 -0400

    chore(none): removed .idea

---
+++ src/index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import "@babel/polyfill";
+import { generateComponents } from "./scripts";
+import { build } from "./scripts/rollup";
+import * as fs from 'fs';
+const program = require("commander");
+
+const { version } = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+
+program
+  .version(version)
+  .command("transpile <dir>", "Transpile components in this directory")
+  .option("-b", "--bundle", "Bundle components")
+  .action(async (dir, { B }) => {
+    await generateComponents(dir);
+
+    if (B) {
+      await build();
+    }
+  })
+  .parse(process.argv);
